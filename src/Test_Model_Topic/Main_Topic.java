12eff4a740c7769329059d24ee15432b70c6bec6
舞姬秀武技 <96943069+jokerme115@users.noreply.github.com>
2022-06-09 20:38:52 +0800

    Update Main_Topic.java

--- src/Test_Model_Topic/Main_Topic.java
+++ src/Test_Model_Topic/Main_Topic.java
@@ -16,11 +16,11 @@ public class Main_Topic {
 		int Count = 5;
 		int score1 = 5;
 		int score2 = 3;
+		int sum = 0;
 		
 		int [] totalScore = new int[Count];
 		ArrayList<Topic> Topic = new ArrayList<>();
 		Scanner scan = new Scanner(System.in);
-		topic_util util = new topic_util();
 		jdbc_util util2 = new jdbc_util();
 		Connection conn = util2.getCon();
 		
@@ -33,25 +33,29 @@ public class Main_Topic {
 
 		while (ItTopic.hasNext()) {
 			Topic topic = ItTopic.next();
-			System.out.println("第" + n+1 +"题目:"  + topic.getTopic());
+			System.out.println("第" + (n+1) +"题:"  + topic.getTopic());
 			System.out.println("A:" + topic.getOption_A());
 			System.out.println("B:" + topic.getOption_B());
 			System.out.println("C:" + topic.getOption_C());
 			System.out.println("D:" + topic.getOption_D());
 			
 			int result = scan.nextInt();
-			System.out.println(topic_util.jugementOption(result, topic));
-//			if (Type == 1) {
-//				int result = scan.nextInt();
-//				totalScore[n] =  topic_util.jugementOption(result, topic);
-//				System.out.println(totalScore[n]);
-//			}else {
-//				
-//			}
+			totalScore[n] =  topic_util.jugementOption(result, topic);
+			System.out.println(totalScore[n]);
 			n++;
 		}
 		//答题后记录分数(0: 0分 1:半对 2:满分)
-		
+		for (int e : totalScore) {
+			if (e == 1) {
+				e = score2;
+			}else if (e == 2) {
+				e = score1;
+			}
+			sum += e;
+		}
 		//答题结束显示分数以及保存分数
+		
+		System.out.println("您的成绩为:" + sum);
+		scan.close();
 	}
 }
